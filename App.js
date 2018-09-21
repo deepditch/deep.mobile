50da4b425c7546b0403b92afb49c02971e5b80f4
drakesvoboda <drakesvoboda@gmail.com>
2018-09-21 18:04:48 -0400

    Fix indentation If you are in visual studio code I've just been using the auto formatting with alt+shift+f

--- App.js
+++ App.js
@@ -1,90 +1,85 @@
-import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, Dimensions, View} from 'react-native';
-import loginPage from './Source/Login/loginPage';        //import the js files you create here.
-import {StackNavigator} from 'react-navigation';
-import Camera from 'react-native-camera';
+import React, { Component } from "react";
+import { Platform, StyleSheet, Text, Dimensions, View } from "react-native";
+import loginPage from "./Source/Login/loginPage"; //import the js files you create here.
+import { StackNavigator } from "react-navigation";
+import Camera from "react-native-camera";
 
-
-class HomeScreen extends React.Component
-{
-  static navigationOptions = 
-    {
-    title: 'Home',
-    };
-  render ()
-  {
-    const {navigate} = this.props.navigation;
-    return(
+class HomeScreen extends React.Component {
+  static navigationOptions = {
+    title: "Home"
+  };
+  render() {
+    const { navigate } = this.props.navigation;
+    return (
       <View style={styles.homeFormat}>
-      <Text
-        onPress = {()=> navigate('Camera')}> GO TO CAMERA.
-      </Text>
+        <Text onPress={() => navigate("Camera")}> GO TO CAMERA.</Text>
       </View>
     );
   }
 }
 
-
-class CameraScreen extends React.Component
-{
-  static navigationOptions = 
-    {
-    title: 'Camera',
-    };
-  render ()
-  {
-    const {navigate} = this.props.navigation;
-    return(
-     <Camera ref={cam => { this.camera = cam; }} style={styles.preview} aspect={Camera.constants.Aspect.fill}> 
-        <Text style={styles.capture} onPress={this.takePicture.bind(this)}> [CAPTURE] </Text>
-       </Camera>
-      );
-    }
-  
-    takePicture() { this.camera.capture().then((data) => console.log(data)).catch(err => console.error(err)); }
-}
-
-const NavApp = StackNavigator({
-  Home:{screen: HomeScreen},
-  Camera:{screen: CameraScreen},
-});
-
-
-
-export default class App extends Component {
+class CameraScreen extends React.Component {
+  static navigationOptions = {
+    title: "Camera"
+  };
   render() {
+    const { navigate } = this.props.navigation;
     return (
-      <NavApp/>
-      // <Camera ref={cam => { this.camera = cam; }} style={styles.preview} aspect={Camera.constants.Aspect.fill}> 
-      //   <Text style={styles.capture} onPress={this.takePicture.bind(this)}> [CAPTURE] </Text>
-      // </Camera>
+      <Camera
+        ref={cam => {
+          this.camera = cam;
+        }}
+        style={styles.preview}
+        aspect={Camera.constants.Aspect.fill}
+      >
+        <Text style={styles.capture} onPress={this.takePicture.bind(this)}>
+          {" "}
+          [CAPTURE]{" "}
+        </Text>
+      </Camera>
     );
   }
 
-  //takePicture() { this.camera.capture().then((data) => console.log(data)).catch(err => console.error(err)); }
+  takePicture() {
+    this.camera
+      .capture()
+      .then(data => console.log(data))
+      .catch(err => console.error(err));
+  }
+}
+
+const NavApp = StackNavigator({
+  Home: { screen: HomeScreen },
+  Camera: { screen: CameraScreen }
+});
+
+export default class App extends Component {
+  render() {
+    return <NavApp />;
+  }
 }
 
 const styles = StyleSheet.create({
-  preview: { 
-    flex: 1, 
-    justifyContent: 'flex-end', 
-    alignItems: 'center', 
-    height: Dimensions.get('window').height, 
-    width: Dimensions.get('window').width
+  preview: {
+    flex: 1,
+    justifyContent: "flex-end",
+    alignItems: "center",
+    height: Dimensions.get("window").height,
+    width: Dimensions.get("window").width
   },
-  homeFormat: { 
-    flex: 1, 
-    justifyContent: 'center', 
-    alignItems: 'center', 
-    height: Dimensions.get('window').height, 
-    width: Dimensions.get('window').width
+  homeFormat: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    height: Dimensions.get("window").height,
+    width: Dimensions.get("window").width
   },
   capture: {
-    flex: 0, 
-    backgroundColor: '#ffffff', 
-    borderRadius: 5, 
-    color: '#000', 
-    padding: 10, 
+    flex: 0,
+    backgroundColor: "#ffffff",
+    borderRadius: 5,
+    color: "#000",
+    padding: 10,
     margin: 40
   }
 });
--- Source/Login/loginPage.js
+++ Source/Login/loginPage.js
@@ -1,23 +1,19 @@
-import React, {Component} from 'react';
-import { StyleSheet, Text, Dimensions, View, Navigator} from 'react-native';
-
-
-
+import React, { Component } from "react";
+import { StyleSheet, Text, Dimensions, View, Navigator } from "react-native";
 
 export default class loginPage extends Component {
   render() {
     return (
       <View style={StyleSheet.container}>
-      <Text> TESTING THE DAMN LOGIN PAGE</Text>
+        <Text>TESTING THE DAMN LOGIN PAGE</Text>
       </View>
     );
   }
-
 }
 
 const style = StyleSheet.create({
-    container:{
-        flex:1,
-        backgroundColor: '#3498db'
-    }
+  container: {
+    flex: 1,
+    backgroundColor: "#3498db"
+  }
 });
