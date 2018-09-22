b9610776051f4570e4f0bd51d0652f54cf09ff51
tanna <safayeth.khan@gmail.com>
2018-09-21 23:14:26 -0400

    Reorganized the app.js, changed home page name to login, moved login information page to loginPage.
    Reorganized the app.js, added some comments, moved section of app.js to loginPage.js. Cleaned some commented codes.
    
    for loginPage.js
    added placeholders for username and password, which take inputs. added a touchable login button.

--- App.js
+++ App.js
@@ -1,90 +1,71 @@
-import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, Dimensions, View} from 'react-native';
+import React, { Component } from 'react';
+import { Platform, StyleSheet, Text, Dimensions, View } from 'react-native';
 import loginPage from './Source/Login/loginPage';        //import the js files you create here.
-import {StackNavigator} from 'react-navigation';
+import { StackNavigator } from 'react-navigation';
 import Camera from 'react-native-camera';
 
 
-class HomeScreen extends React.Component
-{
-  static navigationOptions = 
+
+class CameraScreen extends React.Component {
+  static navigationOptions =
     {
-    title: 'Home',
+      title: 'Camera',
     };
-  render ()
-  {
-    const {navigate} = this.props.navigation;
-    return(
-      <View style={styles.homeFormat}>
-      <Text
-        onPress = {()=> navigate('Camera')}> GO TO CAMERA.
-      </Text>
-      </View>
+  render() {
+    //const { navigate } = this.props.navigation;  
+    // can use the navigate as a onpress to start
+    // navigating to directed page. See loginPage.js.
+    return (
+      <Camera ref={cam => { this.camera = cam; }} style={styles.preview} aspect={Camera.constants.Aspect.fill}>
+        <Text style={styles.capture} onPress={this.takePicture.bind(this)}> [CAPTURE] </Text>
+      </Camera>
     );
   }
+
+  takePicture() { this.camera.capture().then((data) => console.log(data)).catch(err => console.error(err)); }
 }
 
-
-class CameraScreen extends React.Component
-{
-  static navigationOptions = 
-    {
-    title: 'Camera',
-    };
-  render ()
-  {
-    const {navigate} = this.props.navigation;
-    return(
-     <Camera ref={cam => { this.camera = cam; }} style={styles.preview} aspect={Camera.constants.Aspect.fill}> 
-        <Text style={styles.capture} onPress={this.takePicture.bind(this)}> [CAPTURE] </Text>
-       </Camera>
-      );
-    }
-  
-    takePicture() { this.camera.capture().then((data) => console.log(data)).catch(err => console.error(err)); }
-}
+//=============//
+//Basically using stacknavigatior to create 
+//the pages to navigate to one page to another.
+//=============//
 
 const NavApp = StackNavigator({
-  Home:{screen: HomeScreen},
-  Camera:{screen: CameraScreen},
+  Home: { screen: loginPage },        //calls the loginPage from loginPage.js.
+  Camera: { screen: CameraScreen },   // calls the camera screen from above, should be moved to its own .js later.
 });
 
 
 
-export default class App extends Component {
+export default class App extends React.Component {
   render() {
     return (
-      <NavApp/>
-      // <Camera ref={cam => { this.camera = cam; }} style={styles.preview} aspect={Camera.constants.Aspect.fill}> 
-      //   <Text style={styles.capture} onPress={this.takePicture.bind(this)}> [CAPTURE] </Text>
-      // </Camera>
+      <NavApp />
     );
   }
-
-  //takePicture() { this.camera.capture().then((data) => console.log(data)).catch(err => console.error(err)); }
 }
 
 const styles = StyleSheet.create({
-  preview: { 
-    flex: 1, 
-    justifyContent: 'flex-end', 
-    alignItems: 'center', 
-    height: Dimensions.get('window').height, 
+  preview: {
+    flex: 1,
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+    height: Dimensions.get('window').height,
     width: Dimensions.get('window').width
   },
-  homeFormat: { 
-    flex: 1, 
-    justifyContent: 'center', 
-    alignItems: 'center', 
-    height: Dimensions.get('window').height, 
+  homeFormat: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: Dimensions.get('window').height,
     width: Dimensions.get('window').width
   },
   capture: {
-    flex: 0, 
-    backgroundColor: '#ffffff', 
-    borderRadius: 5, 
-    color: '#000', 
-    padding: 10, 
+    flex: 0,
+    backgroundColor: '#ffffff',
+    borderRadius: 5,
+    color: '#000',
+    padding: 10,
     margin: 40
   }
 });
--- Source/Login/loginPage.js
+++ Source/Login/loginPage.js
@@ -1,14 +1,65 @@
-import React, {Component} from 'react';
-import { StyleSheet, Text, Dimensions, View, Navigator} from 'react-native';
+import React, { Component } from 'react';
+import {
+  StyleSheet,
+  Text,
+  Dimensions,
+  View,
+  TextInput,
+  KeyboardAvoidingView,
+  TouchableOpacity,
+  AsyncStorage
+} from 'react-native';
+import { StackNavigator } from 'react-navigation';
 
+/* ================================================
+Login page informatioon
+===================================================
+Basic login page.
+Has a username and password placeholder (currently doesnt do anything but take input)
+Has a login button that when clicked takes user to the camera screen.
 
+=====================TO DO========================
+*Sign up button.
+*authentication
+*Hide password with ***
+*forgot password button
+*connect to database
+*verify session
+*only allow user to access camera when logged in
+*/
 
+export default class loginPage extends React.Component {
+  static navigationOptions =
+    {
+      title: 'Login Screen',
+    };
 
-export default class loginPage extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: '',
+      password: '',
+    }
+  }
+
   render() {
+    const { navigate } = this.props.navigation;
     return (
-      <View style={StyleSheet.container}>
-      <Text> TESTING THE DAMN LOGIN PAGE</Text>
+      <View>
+
+        <TextInput
+          placeholder='Username'
+          onChangeText={(username) => this.setState({ username })}
+        ></TextInput>
+        <TextInput placeholder="Password"
+          onChangeText={(password) => this.setState({ password })}
+        ></TextInput>
+       
+        <TouchableOpacity
+          onPress={() => navigate('Camera')}>
+          <Text>Log In</Text>
+        </TouchableOpacity>
+
       </View>
     );
   }
@@ -16,8 +67,8 @@ export default class loginPage extends Component {
 }
 
 const style = StyleSheet.create({
-    container:{
-        flex:1,
-        backgroundColor: '#3498db'
-    }
+  container: {
+    flex: 1,
+    backgroundColor: '#3498db'
+  }
 });
