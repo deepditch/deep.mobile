c22d0e9aa5ed75bc2eb1279ca574f193731967c3
safayeth <safayeth.khan@gmail.com>
2018-11-14 15:04:09 -0500

    Update source/mapScreen.js
    This is supposed to add map markers. Currently It doesn't work. It is not looping through the Json correctly/setting the possition correctly when button is pressed.

--- source/mapScreen.js
+++ source/mapScreen.js
@@ -5,7 +5,9 @@ import {
   View,
   TextInput,
   TouchableOpacity,
-  KeyboardAvoidingView
+  KeyboardAvoidingView,
+  Button,
+
 } from "react-native";
 import { StackNavigator, DrawerNavigator } from "react-navigation";
 
@@ -18,6 +20,10 @@ export default class mapScreenView extends Component {
      headerLeft:
   <Text onPress={()=>props.navigation.openDrawer()}>Menu</Text>
 };}
+
+state={
+  MarkerPositions:[]
+};
 
 //   constructor(props){
 //       super(props);
@@ -37,12 +43,66 @@ export default class mapScreenView extends Component {
 
 
 
+// async mapMarker() {
+//   const data = {
+//   };
+
+//   const config = {
+//     headers: {
+//       Accept: "application/json",
+//       "Content-Type": "application/json",
+//      // "x-requested-with": "XMLHttpRequest"
+//     },
+//     body: JSON.stringify(data)
+//   };
+
+//   return new Promise((resolve, reject) => {
+//     fetch("http://216.126.231.155/api/road-damage", config)
+//       .then(this.parseJSON)
+//       .then(response => {
+//         if (!response.ok) return reject(response.json);
+//         this.setToken(response.json.access_token);
+//         return resolve(response.json);
+//       });
+//   });
+// }
+
+
+
+// ON button press supposed to run this
+getDamagesHandler=()=>{
+  fetch("http://216.126.231.155/api/road-damage") //get the response from api
+  .then(res=>res.json())
+  .then(parsedRes=>{
+    const MapMarkerArray =[]; //array to store the positions and loop through. 
+    for(const key in parsedRes){    
+      MapMarkerArray.push({                 
+        latitude: parsedRes[key].latitude,
+        longitude: parsedRes[key].longitude,
+        id:key
+      });
+    }
+    this.setState({
+      MarkerPositions:MapMarkerArray
+    })
+  })
+};
+
   render() {
+    // a button to get the cords and place markers when pressed.
+    <View>
+    <Button title="Get Damages" onPress={this.getDamagesHandler}/> 
+  </View>
+  const MarkersP = props.MarkerPositions.map(markerPosition =>(  // markerpositions supposed to have the coords, lat/lang
+    <MapView.Marker coordinate = {markerPosition} key ={userPlace.id}/>
+  ));
     return (
-      <MapView style={style.mappingview}
+      <MapView style={style.mappingview} //MarkersP is supposed to mark the the map with pins
       showsUserLocation
-      followsUserLocation 
-      />
+      followsUserLocation>
+       {MarkersP}           
+      </MapView>
+
 
     );
   }
